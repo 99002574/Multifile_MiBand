02b5d66a3aeced87f620513781b42bc7228ae8cc
99002574 <aswath.jnair@ltts.com>
2020-11-03 16:49:37 +0530

    2nd function

--- heart.h
+++ heart.h
@@ -5,53 +5,67 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include "pbPlots.hpp"
+#include "supportLib.hpp"
 using namespace std;
 vector<string> database;
 string word;
 string line;
-double avgData[24]; 
-int count=0;
+double data[25]; 
+int count;
 int sum=0;
 int sumAvg;
 long int i,j;
-int begTime, endTime;
-string fname;
+//int begTime, endTime;
+//string fname;
 fstream fin;
-// int file_open(string str, fstream fin){
+int generateReport(int begn, string file){
     
-//     fin.open(str.c_str(), ios::in);
-//     if(!fin.is_open()){
-//         return -1;
-//     }
-//     else
-//         return 0; 
-// }
-// int file_close(fstream fin){
-//     fin.close();
-//     if(fin.is_open()){
-//         return -1;
-//     }
-//     else return 0;
-// }
-// int read_file(fstream fin){
-//     while(!fin.eof()){
-//     getline(fin, line); 
-//     stringstream s(line);
     
-//     while (getline(s, word, ',')) { 
-//         database.push_back(word); 
-//     }
-//     }
-//     return 0;
+    count=0;
+    fin.open(file.c_str(), ios::in);
+    if(!fin.is_open()){
+        return -1;
+    }
+    while(!fin.eof()){
+    getline(fin, line); 
+    stringstream s(line);
+    
+    while (getline(s, word, ',')) { 
+        database.push_back(word); 
+    }
+    }
+    
+    for (i = 0; i < database.size()-1; i++){
+        if(i%4==0){
+            if(stoi(database[i+3]) >= begn && count<=25){
+                data[count]=stoi(database[i];
+                count++;
+                if(count>25) break;
+            }
+            
+        }
+    }
+    RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
+    vector<double> x{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
+    vector<double> y{data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20],data[21],data[22],data[23],data[24]};
+    DrawScatterPlot(imageRef, 600, 400, &x, &y);
+    vector<double> *pngData = ConvertToPNG(imageRef->image);
+    WriteToFile(pngData, "plot.png");
+    DeleteImage(imageRef->image);
+    //cout<<count<<endl;
+    fin.close();
 
-// }
-void display(){
-    cout<<"Enter the beginning time : "<<endl;
-    cin>>begTime;
-    cout<<"Enter the end time : "<<endl;
-    cin>>endTime;
-    cout<<"Enter the filename : "<<endl;
-    cin>>fname;
+}
+
+
+void display(int begTime, int endTime, string fname){
+    // cout<<"Enter the beginning time : "<<endl;
+    // cin>>begTime;
+    // cout<<"Enter the end time : "<<endl;
+    // cin>>endTime;
+    // cout<<"Enter the filename : "<<endl;
+    // cin>>fname;
     
     
     fin.open(fname.c_str(), ios::in);
@@ -68,12 +82,12 @@ void display(){
         if(i%4==0){
             if(stoi(database[i+3]) >= begTime && stoi(database[i+3]) <= endTime){
                 cout<<"\n"<<database[i]<<endl;
-                count++;
+                //count++;
             }
             
         }
     }
-    cout<<count<<endl;
+    //cout<<count<<endl;
     fin.close();
 
 }
