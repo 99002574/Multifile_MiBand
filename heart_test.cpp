d323943ad00f9eed7e777b9c6561697c7af288bd
99002574 <aswath.jnair@ltts.com>
2020-11-03 21:59:26 +0530

    test file v1

---
+++ heart_test.cpp
@@ -0,0 +1,21 @@
+#include "heart.h"
+#include <gtest/gtest.h>
+namespace {
+
+TEST(heart_test, displayBetween) {
+  
+  EXPECT_EQ(34, displayBetween(75038,75157, "heart_data_csv.csv"));
+}
+TEST(heart_test, generateReport {
+  
+  EXPECT_EQ(0, generateReport(112346,"heart_data_csv.csv"));
+}
+TEST(heart_test, highHeartRate {
+  
+  EXPECT_EQ(1812, highHeartRate(100, "heart_data_csv.csv"));
+}
+TEST(heart_test, exerciseZoneMax) {
+  
+  EXPECT_EQ(140, exerciseZoneMax(25,"heart_data_csv.csv"));
+}
+} // namespace
