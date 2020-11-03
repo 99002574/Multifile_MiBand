8119a69597513531ea3fc32088db380954f2d647
99002574 <aswath.jnair@ltts.com>
2020-11-03 17:02:33 +0530

    length adjusted for image

--- heart.h
+++ heart.h
@@ -47,9 +47,9 @@ int generateReport(int begn, string file){
         }
     }
     RGBABitmapImageReference *imageRef = CreateRGBABitmapImageReference();
-    vector<double> x{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
+    vector<double> x{80,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24};
     vector<double> y{data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14],data[15],data[16],data[17],data[18],data[19],data[20],data[21],data[22],data[23],data[24]};
-    DrawScatterPlot(imageRef, 600, 400, &x, &y);
+    DrawScatterPlot(imageRef, 1200, 400, &x, &y);
     vector<double> *pngData = ConvertToPNG(imageRef->image);
     WriteToFile(pngData, "plot.png");
     DeleteImage(imageRef->image);
