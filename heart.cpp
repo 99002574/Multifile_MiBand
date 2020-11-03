e4c769f7a5e3d1cea6fbbe070504934887634eb1
99002574 <aswath.jnair@ltts.com>
2020-11-03 17:35:26 +0530

    updated main

--- heart.cpp
+++ heart.cpp
@@ -1,8 +1,8 @@
 #include "heart.h"
 using namespace std;
 int main(){
-    cout<<displayBetween(75038,75157, "heart_data_csv.csv");
-    cout<<generateReport(103529,"heart_data_csv.csv");
+    cout<<displayBetween(75038,75157, "heart_data_csv.csv")<<endl;
+    cout<<generateReport(103529,"heart_data_csv.csv")<<endl;
 
 
 
