eaf266da3769de1aeeb41610996a36880bb3eed9
99002574 <aswath.jnair@ltts.com>
2020-11-04 11:44:11 +0530

    updation to code

--- heart_test.cpp
+++ heart_test.cpp
@@ -4,6 +4,11 @@ namespace {
 TEST(heart_test, displayBetween) {  
   EXPECT_EQ(34, displayBetween(75038,75157, "heart_data_csv.csv"));
 }
+TEST(heart_test, fileNotFound){
+  EXPECT_EQ(-1, displayBetween(75038,75157, "random.csv"));
+  EXPECT_EQ(-1, highHeartRate(100, "random.csv"));
+  EXPECT_EQ(-1, exerciseZoneMax(25,"random.csv"));
+}
 
 TEST(heart_test, highHeartRate) {  
   EXPECT_EQ(1208, highHeartRate(100, "heart_data_csv.csv"));
